626eb06ad273eb76846384105db5d3873426af32
RioProjectX <88654855+RioProjectX@users.noreply.github.com>
2021-09-02 18:27:47 +0700

    Update videoplayer.py

--- bot/videoplayer.py
+++ bot/videoplayer.py
@@ -14,33 +14,33 @@ VIDEO_CALL = {}
 async def stream(client, m: Message):
     replied = m.reply_to_message
     if not replied:
-        await m.reply("😕 **sorry it's not a video**\n\n» use the /stream command by replying to the video.")
+        await m.reply("😕 **Maaf Itu Bukan Video**\n\n» Tulis /stream Dengan reply pesan ke video.")
     elif replied.video or replied.document:
-        msg = await m.reply("🔁 **downloading video...**\n\n❔ this process will take quite a while depending on the size of the video.")
+        msg = await m.reply("🔁 **Mendownlaod Video...**\n\n❔ Proses ini tergantung oleh berapa besar size video.")
         chat_id = m.chat.id
         try:
             video = await client.download_media(m.reply_to_message)
             await msg.edit("♻ **Converting...**")
             os.system(f'ffmpeg -i "{video}" -vn -f s16le -ac 2 -ar 48000 -acodec pcm_s16le -filter:a "atempo=0.81" vid-{chat_id}.raw -y')
         except Exception as e:
-            await msg.edit(f"**🚫 Error** - `{e}`")
+            await msg.edit(f"**🚫 Eror** - `{e}`")
         await asyncio.sleep(5)
         try:
             group_call = group_call_factory.get_file_group_call(f'vid-{chat_id}.raw')
             await group_call.start(chat_id)
             await group_call.set_video_capture(video)
             VIDEO_CALL[chat_id] = group_call
-            await msg.edit("**💡 video streaming started !\n\n❕ join to video chat to watch the video.**")
+            await msg.edit("**💡 video streaming dimulai !\n\n❕ join ke video chat untuk menonton video.**")
         except Exception as e:
             await msg.edit(f"**Error** -- `{e}`")
     else:
-        await m.reply("`please reply to a video !`")
+        await m.reply("`tolong reply pesan ke video !`")
 
 @Client.on_message(filters.command("stop"))
 async def stopvideo(client, m: Message):
     chat_id = m.chat.id
     try:
         await VIDEO_CALL[chat_id].stop()
-        await m.reply("**⏹️ streaming has ended !**\n\n✅ __userbot has been disconnected from the video chat__")
+        await m.reply("**⏹️ streaming telah di hentikan !**\n\n✅ __userbot has been disconnected from the video chat__")
     except Exception as e:
         await m.reply(f"**🚫 Error** - `{e}`")
